2872858d3ba6a835dd930d72a631fe962c04d94c
Jan Tojnar <jtojnar@gmail.com>
2021-04-28 01:53:12 +0200

    mac: Fix nix profile activation

--- script-mac.sh
+++ script-mac.sh
@@ -1,5 +1,9 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
+
+# The file lacks final new line, breaking Nix’s appending into the profile.
+echo >> ~/.bash_profile
+
 sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
 source ./script-common.sh
