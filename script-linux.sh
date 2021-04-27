2c01c7add57947c8f8c598718419827a84fc668a
Jan Tojnar <jtojnar@gmail.com>
2021-04-27 19:17:43 +0200

    Install Nix

--- script-linux.sh
+++ script-linux.sh
@@ -1,1 +1,2 @@
 echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
+curl -L https://nixos.org/nix/install | sh
--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,4 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
+sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
