dd59f045d77f53b5749b5158a08778f574dc689a
Jan Tojnar <jtojnar@gmail.com>
2021-04-28 01:53:35 +0200

    install nano from Nix
    The mac one is ancient

--- script-common.sh
+++ script-common.sh
@@ -4,5 +4,10 @@ nix-env -i \
     -A exa \
     -A fd \
     -A gh \
+    -A nano \
     -A ripgrep \
     -f channel:nixos-unstable
+
+# Enable nano syntax highlighting
+mkdir -p ~/.config/nano
+echo 'include "'$(nix-build channel:nixos-unstable --no-out-link -A nano)'/share/nano/*.nanorc"' >> ~/.config/nano/nanorc
