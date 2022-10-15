f6abc06a7e91d64f21d86ab7e9909e90254aa073
Jan Tojnar <jtojnar@gmail.com>
2022-10-15 10:44:31 +0200

    Do not add channels
    They are slow and we  will use flakes instead.

--- script-linux.sh
+++ script-linux.sh
@@ -1,3 +1,3 @@
 echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
-curl -L https://nixos.org/nix/install | sh
+sh <(curl -L https://nixos.org/nix/install) --no-channel-add
 source ./script-common.sh
--- script-mac.sh
+++ script-mac.sh
@@ -5,5 +5,5 @@ echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac in
 # The file lacks final new line, breaking Nix’s appending into the profile.
 echo >> ~/.bash_profile
 
-sh <(curl -L https://nixos.org/nix/install)
+sh <(curl -L https://nixos.org/nix/install) --no-channel-add
 source ./script-common.sh
