98ffac3593d4004434618812ca441137929f112d
Jan Tojnar <jtojnar@gmail.com>
2022-10-15 10:35:17 +0200

    mac: Fix warning
    Warning: the flag --darwin-use-unencrypted-nix-store-volume
             is no longer needed and will be removed in the future.

--- script-mac.sh
+++ script-mac.sh
@@ -5,5 +5,5 @@ echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac in
 # The file lacks final new line, breaking Nix’s appending into the profile.
 echo >> ~/.bash_profile
 
-sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
+sh <(curl -L https://nixos.org/nix/install)
 source ./script-common.sh
